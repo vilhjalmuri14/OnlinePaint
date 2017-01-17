80acae105b8e7ad143a0163dd3f9ed1f3d41dbcb
vilhjalmuri14 <vilhjalmuri14@ru.is>
2017-01-17 16:10:22 +0000

    removed unnecessary comments

--- js/main.js
+++ js/main.js
@@ -1,59 +1,3 @@
-/* 
-$(document).ready(function(){
-	var canvas = document.getElementById("myCanvas");
-	var context = canvas.getContext("2d");
-
-
-	var startX = 0;
-	var startY = 0;
-	var isDrawing = false;
-
-	$("#myCanvas").mousedown(function(e){
-		//console.log("inside mousedown");
-
-		var x = e.pageX - this.offsetLeft;
-		var y = e.pageY - this.offsetTop;
-		
-		startX = x;
-		startY = y;
-
-		isDrawing = true;
-
-		//console.log("X: " + x + " Y: " + y);
-
-		//context.fillStyle = "blue";
-		//context.fillRect(x - 30, y - 30, 60, 60);
-		//context.strokeStyle = "red";
-		//context.strokeRect(x - 30, y - 30, 60, 60);
-
-		
-	});
-
-	$("#myCanvas").mousemove(function(e){
-		if( isDrawing === true ) {
-			var x = e.pageX - this.offsetLeft;
-			var y = e.pageY - this.offsetTop;  
-
-			// clearing the screen
-			context.clearRect(0,0,500,500);
-
-			context.beginPath();
-			context.moveTo(startX, startY);
-			context.lineTo(x, y);
-			context.stroke();
-		}
-	});
-
-	$("#myCanvas").mouseup(function(e){
-		
-
-		isDrawing = false;
-	});
-});
-*/
-
-// the code from lecture
-
 class Shape {
 	constructor(x, y, color) {
 		this.startX = x;
@@ -102,7 +46,6 @@ class Circle extends Shape {
 	}
 
 	draw(context) {
-		// Drawing a filled circle with outline:
 		context.beginPath();
 
 		var radius = Math.sqrt(Math.pow(Math.abs(this.startX - this.endX), 2) 
@@ -128,7 +71,8 @@ class Pen extends Shape {
 	}
 
 	draw(context) {
-			
+		// TODO: loop through the points and print
+
 	}
 }
 
